18af7084fd1e71cb84dc74996600ae9863c5a850
Rishavraaj <rishavrajpatna7@gmail.com>
2025-01-16 09:14:00 +0530

    feat: added shadcn ui

---
+++ src/app/_components/setup-webhook-button.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const SetupWebhookButton = () => {
+  return (
+    <div>
+      <Button>Setup Webhook</Button>
+    </div>
+  );
+};
+
+export default SetupWebhookButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { ConnectCalendarButton } from "./_components/ConnectCalendarButton";
 import { HydrateClient } from "@/trpc/server";
+import SetupWebhookButton from "./_components/setup-webhook-button";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-        <h1 className="text-4xl font-bold">AI Calendar Assistant</h1>
-        <p className="text-lg text-gray-400">
-          Connect your Google Calendar to get started
-        </p>
-        <ConnectCalendarButton />
+        <SetupWebhookButton />
       </main>
     </HydrateClient>
   );
